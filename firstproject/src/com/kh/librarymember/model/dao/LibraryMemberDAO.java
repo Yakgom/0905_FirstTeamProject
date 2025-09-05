e6533a8925bd2715558a6a885f5524144fe4dd00
0901_JDBC <yoonja486@gmail.com>
2025-09-05 15:26:01 +0900

    HGJ

--- firstproject/src/com/kh/librarymember/model/dao/LibraryMemberDAO.java
+++ firstproject/src/com/kh/librarymember/model/dao/LibraryMemberDAO.java
@@ -1,5 +1,64 @@
 package com.kh.librarymember.model.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.kh.common.JDBCTemplate;
+import com.kh.librarymember.model.vo.LibraryMember;
+
 public class LibraryMemberDAO {
 
+	private Properties prop = new Properties();
+	
+	
+	public LibraryMemberDAO() {
+		
+		try {
+			prop.loadFromXML(new FileInputStream("resources/libarymember-mapper.xml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	System.out.println(prop + "test");
+	}
+	
+	
+	public int memberInsert(Connection conn, LibraryMember lm) {
+		
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		try {
+			prop.loadFromXML(new FileInputStream("resources/libarymember-mapper.xml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String sql = prop.getProperty("memberInsert");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, lm.getMemberName());
+			pstmt.setString(2, lm.getMemberPhone());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(pstmt);
+		}
+		
+		
+		return result;
+		
+	}
+	
+	
+	
+	
 }
--- firstproject/src/com/kh/librarymember/view/LibraryView.java
+++ firstproject/src/com/kh/librarymember/view/LibraryView.java
@@ -1,5 +1,64 @@
 package com.kh.librarymember.view;
 
+import java.util.Scanner;
+
+import com.kh.librarymember.controller.LibraryMemberController;
+
 public class LibraryView {
 
+	private Scanner sc = new Scanner(System.in);
+	private LibraryMemberController lmc = new LibraryMemberController();
+	
+	
+	public void mainMenu() {
+		
+		/*
+		 * 1. 회원 가입- 이름, 전화번호
+		 * 2. 회원 정보변경(전화번호)
+		 * 3. 회원 조회
+		 * 4. 회원 탈퇴
+		 */
+		
+		while(true) {
+			System.out.println();
+			System.out.println("===== 🙎‍♂️🙎‍♂️ 회원 관리 서비스입니다 🙎‍♀️🙎 =====");
+			System.out.println();
+			System.out.println("1. 회원 가입");
+			System.out.println("2. 회원 정보 변경");
+			System.out.println("3. 회원 정보 조회");
+			System.out.println("4. 회원 탈퇴");
+			System.out.println("0. 메인메뉴로 돌아가기");
+			System.out.print("메뉴를 선택해주세요 > ");
+			int menuNo = sc.nextInt();
+			sc.nextLine();
+			
+			
+			switch(menuNo) {
+			case 1 : memberInsert(); break;
+			case 2 : break;
+			case 3 : break;
+			case 4 : break;
+			case 0 : System.out.println("메인화면으로 이동합니다"); return;
+			}
+		}
+	}
+	
+	
+	private void memberInsert() {
+		
+		System.out.println();
+		System.out.println("===== 📃📃 회원 가입 서비스 📃📃 =====");
+		System.out.println("등록하실 이름을 적어주세요 > ");
+		String memberName = sc.nextLine();
+		System.out.println("등록하실 전화번호를 적어주세요 > ");
+		String memberPhone = sc.nextLine();
+		
+	}
+	
+	
+	
+	
+	
+	
 }
+
